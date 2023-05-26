e65e686924ba489456ca43ad724e02cf593fad76
bobdisappear <dnruiz87@gmail.com>
2023-05-26 19:53:09 +0200

    Agregar archivos de proyecto.

---
+++ practica1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+// variable
+
+int i = 5;
+long l = 32165161231;
+double d = 12.34;
+decimal de = 15.158612m;
+char c = 'a';
+string s ="cosas" ;
+bool ok = true;
+
+Console.WriteLine(d);
+
+//Metodo vacio
+EmptyMethod();  
+void EmptyMethod()
+{
+    Console.WriteLine("Hello"); 
+}
+
+//Metodo numero 
+int sum = Calculations(10, 25);
+Console.WriteLine(sum);
+int Calculations(int a , int b)
+{
+    return a + b;
+}
+
+//Metodo booleano
+bool greater =IsItGreater(20);
+Console.WriteLine(greater);
+bool IsItGreater(int a)
+{
+    return a > 10;
+}
